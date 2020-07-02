161e6baee8cf415b7ecd04a4f71bc79e26b2ecfb
grnstrn <starostingrigory@gmail.com>
2020-07-02 16:21:04 +0300

    Initial commit

---
+++ src/main/java/com/example/sweatapp/SweatApp.java
@@ -0,0 +1,11 @@
+package com.example.sweatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweatApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SweatApp.class, args);
+    }
+}
---
+++ src/main/resources/templates/greeting.ftl
@@ -0,0 +1,7 @@
+<#import "parts/common.ftl" as c>
+
+<@c.page>
+    <h5>Hello, guest</h5>
+    <div>this is like tweeter
+    </div>
+</@c.page>
